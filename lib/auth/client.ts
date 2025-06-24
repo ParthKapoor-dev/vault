0ed6520822306eb1ce4c73b2ce9dbef58689141e
Parth Kapoor <parthkapoor.coder@gmail.com>
2025-06-25 02:33:00 +0530

    feat: better-auth with github init

---
+++ lib/auth/client.ts
@@ -0,0 +1,18 @@
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient();
+
+export const login = async () => {
+  const data = await authClient.signIn.social({
+    provider: "github",
+  });
+  console.log(data);
+};
+
+export const logout = async () => {
+  await authClient.signOut();
+};
+
+export const useSession = () => {
+  return authClient.useSession();
+};
---
+++ lib/auth/index.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { customSession } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const isAdmin = user.email === "pkapoor_be22@thapar.edu" ? true : false;
+      return {
+        user: {
+          ...user,
+          isAdmin,
+        },
+        session,
+      };
+    }),
+  ],
+});
